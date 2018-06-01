a5509463a8a27237d2d00e5a5eaf497cbdc0f9a3
Tech-A27 <tech-a27@tech-a27s-air.lan>
2018-06-01 01:02:34 +0100

    made x and o appear with classes

--- js/demo.js
+++ js/demo.js
@@ -4,9 +4,9 @@ $(document).ready(function(){
 //Variables
   // Target the table
   var $board = $('table');
+  // console.log($(board));
 // Target the td
   var $boxes = $('td');
-// Turns counter to switch X and O turns
   var turns = 0;
   var reset = $('#reset');
 
@@ -15,6 +15,7 @@ $(document).ready(function(){
   console.log($boxes);
 
 $(reset).click(function(event){
+  // console.log(event);
   $boxes.text('');
   $boxes.removeClass();
 })
@@ -26,14 +27,38 @@ $($boxes).click(xos)
   function xos(event){
   if (turns % 2 === 0){
     $(this).text('X').addClass('X');
+    // this.addClass('X')
      turns++;
   } else {
     $(this).text('O').addClass('O');
+    // this.addClass('O');
     turns++;
+    // if (turns === 9){
+    //   //   event.stopImediatePropagation;
+    //   // }
   }
 
 
   console.log(turns);
+  // $(this).text('X');
+  // $(this).addClass('X');
+  // This is that particular box you've clicked
+  // console.log(this.attr());
+  // This is that particular box you've clicked
+
+//   $boxes.click(function(event){
+//     if (turns === 9){
+//     event.stopImediatePropagation;
+//   }
+// })
+}
+
+
+// Remove the div after
+
+
+// boxes.value
+
 
 
 }); // Document Ready Closing
